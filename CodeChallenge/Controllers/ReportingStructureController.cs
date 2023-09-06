15b68c4db10a7071a9480f7beb8eae7d72cace79
Morgan West <mrw6863@rit.edu>
2023-09-06 10:33:28 -0400

    first commit

---
+++ CodeChallenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Services;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("api/reportingStructure")]
+    public class ReportingStructureController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IReportingStructureService _reportingStructureServicee;
+
+        public ReportingStructureController(ILogger<ReportingStructureController> logger, IReportingStructureService reportingStructureService)
+        {
+            _logger = logger;
+            _reportingStructureServicee = reportingStructureService;
+        }
+
+        [HttpGet("{id}", Name = "getReportingStructureById")]
+        public IActionResult GetReportingStructureById(String id)
+        {
+            _logger.LogDebug($"Received reporting structure get request for '{id}'");
+
+            var reportingStructure = _reportingStructureServicee.GetById(id);
+
+            if (reportingStructure == null)
+                return NotFound();
+
+            return Ok(reportingStructure);
+        }
+
+        
+    }
+}
---
+++ CodeChallenge/Services/ReportingStructureService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeChallenge.Models;
+using Microsoft.Extensions.Logging;
+using CodeChallenge.Repositories;
+
+namespace CodeChallenge.Services
+{
+    public class ReportingStructureService : IReportingStructureService
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly ILogger<EmployeeService> _logger;
+
+        public ReportingStructureService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        public ReportingStructure GetById(string id)
+        {
+
+            Employee employee = _employeeRepository.GetById(id);
+
+            if (employee == null) {
+                throw new Exception("Invalid employeeId: " + id);
+            }
+
+            ReportingStructure reportingStructure = new ReportingStructure(employee);
+
+            int numberOfReports = 0;
+            List<Employee> directReports = employee.DirectReports;
+            numberOfReports = directReports.Count();
+            for (Employee e : directReports) {
+                List<Employee> dp = emp.getDirectReports();
+                if (dp != null) {
+                    numberOfReports += emp.getDirectReports().size();
+                }
+            }
+            reportingStructure.setNumberOfReports(numberOfReports);
+
+            return reportingStructure;
+        }
+    }
+}
